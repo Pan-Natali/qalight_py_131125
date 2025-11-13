67630b277250bf63114f6b1689d40705119f3fbd
alex-pancho <pan.pan.4it@gmail.com>
2025-11-13 20:42:43 +0200

    lesson 01

---
+++ lesson01/lesson_01.py
@@ -0,0 +1,30 @@
+print("Hello word an all good man and woman!")
+print("all nice!")
+my_user_name = 'Alex' # str
+# n = "Alex"
+my_user_age  = 45 # int
+my_user_password = """skdjlsdskf"""
+my_money = 123.55 # float
+im_teacher = True # bool
+
+# get_user_name = input("Введіть своє ім'я: ")
+get_user_name = "Natalia"
+greeting = f"Hello, {get_user_name}"
+print(greeting)
+
+size_of_square = "375" 
+size_of_square_int = int(size_of_square)
+print(size_of_square_int + 5, type(size_of_square_int))
+# Будь-який текст, щоб нагадати, що тут було таке
+new_variable = my_user_age + my_money
+print(new_variable)
+print("*" * 88)
+# $ & * (@) ! `` - =
+
+def first_function():
+    "Цей відступ щоб позначити що дії відбуваються"
+    "в самій функції"
+    print("allo")
+    return 1 + 1
+
+first_function()
